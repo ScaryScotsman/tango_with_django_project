7c7c8a8fd353ce6dd3f8935700cb66ac2e7e5516
Michael Mcgrattan <mcgrattanmichael@gmail.com>
2021-04-30 16:09:52 +0100

    Progress for chapter 8 - MMcG 30/04/21

--- rango/forms.py
+++ rango/forms.py
@@ -2,8 +2,7 @@ from django import forms
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,
-                           help_text="Please enter the category name.")
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -17,7 +16,7 @@ class PageForm(forms.ModelForm):
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
-    def clean (self):
+    def clean(self):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
 
@@ -25,7 +24,7 @@ class PageForm(forms.ModelForm):
             url = f'http://{url}'
             cleaned_data['url'] = url
 
-        return cleaned_data
+            return cleaned_data
 
     class Meta:
         model = Page
--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,6 @@ from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to the {{ boldmessage }} in the template!
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
@@ -13,14 +11,10 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    # Return a rendered response to send to the client. We make use of the shortcut function to
-    # make our lives easier. Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Michael'}
-
-    return render(request, 'rango/about.html', context=context_dict)
+    return render(request, 'rango/about.html')
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -28,14 +22,14 @@ def show_category(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
-        context_dict['pages'] = pages
         context_dict['category'] = category
+        context_dict['pages'] = pages
 
     except Category.DoesNotExist:
-        context_dict['pages'] = None
         context_dict['category'] = None
+        context_dict['pages'] = None
 
-    return render(request, 'rango/category.html', context=context_dict)
+    return render(request, 'rango/category.html', context_dict)
 
 def add_category(request):
     form = CategoryForm()
